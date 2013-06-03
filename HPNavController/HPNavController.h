779a4b0ba742725bea2327e0d21c933dba27dd3c
Herve Peroteau <herve.peroteau@gmail.com>
2013-06-03 11:27:50 +0200

    Initial Commit

---
+++ HPNavController/HPNavController.h
@@ -0,0 +1,13 @@
+//
+//  HPNavController.h
+//  HPNavController
+//
+//  Created by Hervé PEROTEAU on 03/06/13.
+//  Copyright (c) 2013 Hervé PEROTEAU. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HPNavController : NSObject
+
+@end
---
+++ HPNavController/HPNavController.m
@@ -0,0 +1,13 @@
+//
+//  HPNavController.m
+//  HPNavController
+//
+//  Created by Hervé PEROTEAU on 03/06/13.
+//  Copyright (c) 2013 Hervé PEROTEAU. All rights reserved.
+//
+
+#import "HPNavController.h"
+
+@implementation HPNavController
+
+@end
